10d1d7bcdfb13b4271c95cefe8af7de8be33b639
IchiharaGaku <gi226bb@ezweb.ne.jp>
2020-05-25 20:55:36 +0900

    master

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('a[href^="#"]').click.each(function () {
+  $('a[href^="#"]').click(function () {
     var adjust = 0;
     var speed = 400;
     var href = $(this).attr("href");
@@ -96,40 +96,59 @@ $(function () {
         }
       });
     });
-
-    $(window).scroll(function () {
-      $(".model1").show(function () {
-        var txtPos = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > txtPos - windowHeight + 200) {
-          $(this).addClass("model1in");
-        }
-      });
-    });
-
-    $(window).scroll(function () {
-      $(".model2").show(function () {
-        var txtPos = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > txtPos - windowHeight + 200) {
-          $(this).addClass("model2in");
-        }
-      });
-    });
-
-    $(window).scroll(function () {
-      $(".model3").show(function () {
-        var txtPos = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > txtPos - windowHeight + 200) {
-          $(this).addClass("model3in");
-        }
-      });
-    });
   }
+  $(window).scroll(function () {
+    $(".room1").show(function () {
+      var txtPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > txtPos - windowHeight + 200) {
+        $(this).addClass("room1in");
+      }
+    });
+  });
+  $(window).scroll(function () {
+    $(".room2").show(function () {
+      var txtPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > txtPos - windowHeight + 200) {
+        $(this).addClass("room2in");
+      }
+    });
+  });
+  $(window).scroll(function () {
+    $(".model1").show(function () {
+      var txtPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > txtPos - windowHeight + 200) {
+        $(this).addClass("model1in");
+      }
+    });
+  });
+
+  $(window).scroll(function () {
+    $(".model2").show(function () {
+      var txtPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > txtPos - windowHeight + 200) {
+        $(this).addClass("model2in");
+      }
+    });
+  });
+
+  $(window).scroll(function () {
+    $(".model3").show(function () {
+      var txtPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > txtPos - windowHeight + 200) {
+        $(this).addClass("model3in");
+      }
+    });
+  });
   $(window).scroll(function () {
     $(".articles").show(function () {
       var txtPos = $(this).offset().top;
@@ -146,4 +165,117 @@ $(function () {
     $(this).toggleClass("active");
     $(".menu-content").toggleClass("open");
   });
+
+  // iPad用jQuery  ----------------------------------------------
+
+  if ($(window).innerWidth() >= 481 && $(window).innerWidth() < 769) {
+    $(window).scroll(function () {
+      $(".hair").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (scroll > 1) {
+          $(this).addClass("fadein");
+        }
+      });
+      $(".style").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (scroll > txtPos - windowHeight) {
+          $(this).addClass("rightin");
+        }
+      });
+
+      // $("#top-btn").offset({ right: 1000 });
+    });
+  }
+
+  //  スマホ用ｊQuery ---------------------------------------
+
+  if ($(window).innerWidth() < 481) {
+    $(window).scroll(function () {
+      $(".style").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (scroll > txtPos - windowHeight) {
+          $(this).addClass("stylein");
+        }
+      });
+    });
+    $(window).scroll(function () {
+      $(".produ").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (scroll > txtPos - windowHeight) {
+          $(this).removeClass("produin");
+        }
+      });
+    });
+    $(window).scroll(function () {
+      $(".style-top").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (scroll > txtPos - windowHeight) {
+          $(this).addClass("fadeon");
+        }
+      });
+    });
+    $(window).scroll(function () {
+      $(".style-bottom").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (scroll > txtPos - windowHeight) {
+          $(this).addClass("fadeon");
+        }
+      });
+    });
+    $(window).scroll(function () {
+      $(".protop").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (scroll > txtPos - windowHeight) {
+          $(this).addClass("fadeon");
+        }
+      });
+    });
+    $(window).scroll(function () {
+      $(".model1").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > txtPos - windowHeight + 100) {
+          $(this).addClass("model1in");
+          $(this).offset({ left: 180 });
+        }
+      });
+    });
+
+    $(window).scroll(function () {
+      $(".model2").show(function () {
+        var txtPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > txtPos - windowHeight + 100) {
+          $(this).addClass("model2in");
+          $(this).offset({ left: 2 });
+        }
+      });
+    });
+
+    $(".main-wrap").css({ height: 1000 });
+    $(".main-wrap").offset({ top: 1300 });
+  }
 });
